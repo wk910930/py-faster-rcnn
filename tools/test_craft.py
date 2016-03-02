c2c5f36781c93fcde157d79a5acc8b3bff7bbe6f
Wang Kun <wk910930@126.com>
2016-03-02 22:26:43 +0800

    add craft related code

---
+++ tools/test_craft.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""Test a Fast R-CNN network on an image database."""
+
+import _init_paths
+from fast_rcnn.craft import test_net
+from fast_rcnn.config import cfg, cfg_from_file
+from datasets.factory import get_imdb
+import caffe
+import argparse
+import pprint
+import time, os, sys
+import cPickle
+import numpy as np
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
+                        default=0, type=int)
+    parser.add_argument('--def', dest='prototxt',
+                        help='prototxt file defining the network',
+                        default=None, type=str)
+    parser.add_argument('--net', dest='caffemodel',
+                        help='model to test',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file', default=None, type=str)
+    parser.add_argument('--wait', dest='wait',
+                        help='wait until net file exists',
+                        default=True, type=bool)
+    parser.add_argument('--imdb', dest='imdb_name',
+                        help='dataset to test',
+                        default='voc_2007_test', type=str)
+    parser.add_argument('--comp', dest='comp_mode', help='competition mode',
+                        action='store_true')
+    parser.add_argument('--bbox_mean', dest='bbox_mean',
+                        help='the mean of bbox',
+                        default=None, type=str)
+    parser.add_argument('--bbox_std', dest='bbox_std',
+                        help='the std of bbox',
+                        default=None, type=str)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    print('Called with args:')
+    print(args)
+
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+
+    print('Using config:')
+    pprint.pprint(cfg)
+
+    while not os.path.exists(args.caffemodel) and args.wait:
+        print('Waiting for {} to exist...'.format(args.caffemodel))
+        time.sleep(10)
+
+    caffe.set_mode_gpu()
+    caffe.set_device(args.gpu_id)
+    net = caffe.Net(args.prototxt, args.caffemodel, caffe.TEST)
+    net.name = os.path.splitext(os.path.basename(args.caffemodel))[0]
+    
+    # apply bbox regression normalization on the net weights
+    with open(args.bbox_mean, 'rb') as f:
+        bbox_means = cPickle.load(f)
+    with open(args.bbox_std, 'rb') as f:
+        bbox_stds = cPickle.load(f)
+    #bbox_means = bbox_means[0:16]
+    #bbox_stds = bbox_stds[0:16]
+
+    net.params['bbox_pred'][0].data[...] = net.params['bbox_pred'][0].data * bbox_stds[:, np.newaxis]
+    net.params['bbox_pred'][1].data[...] = net.params['bbox_pred'][1].data * bbox_stds + bbox_means
+
+
+    imdb = get_imdb(args.imdb_name)
+    imdb.competition_mode(args.comp_mode)
+    
+    test_net(net, imdb)
+
