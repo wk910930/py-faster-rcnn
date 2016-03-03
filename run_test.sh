1d237e39c03878470be72439c4963023e0aa20e3
Kun Wang <wk910930@gmail.com>
2016-03-03 11:04:32 +0800

    change --cfg to local craft.yml

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@
      --def /home/kwang/ilsvrc/fast-rcnn-ilsvrc-test/models/gn_hkbn/test_4d_700.prototxt \
      --net /home/kwang/model_zoo/bn_yangbin_pretrain_xyzeng_proposals_5000_stepsize/models/hkbn_4d_fast_rcnn_iter_110000.caffemodel \
      --imdb ilsvrc_2013_val2 \
-     --cfg /home/kwang/ilsvrc/fast-rcnn-ilsvrc-test/experiments/cfgs/ilsvrc_700.yml \
+     --cfg ./experiments/cfgs/craft.yml \
      --bbox_mean /home/kwang/ilsvrc/fast-rcnn-ilsvrc-test/output/rpn90/bbox_means.pkl \
      --bbox_std /home/kwang/ilsvrc/fast-rcnn-ilsvrc-test/output/rpn90/bbox_stds.pkl
 
