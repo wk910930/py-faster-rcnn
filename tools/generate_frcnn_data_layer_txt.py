4899adb9f413d47f5ea46776aa3b4ba382eceab6
Kun Wang <wk910930@gmail.com>
2016-03-10 09:46:52 +0800

    add generate_frcnn_data_layer_txt tool

---
+++ tools/generate_frcnn_data_layer_txt.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2016 CUHK
+# Written by Wang Kun
+# --------------------------------------------------------
+
+"""Generate txt file as the input to craft::frcnn_train_data_layer"""
+
+import _init_paths
+from datasets.coco import coco
+from roi_data_layer.roidb import prepare_roidb
+from roi_data_layer.roidb import add_bbox_regression_targets
+
+imdb = coco('train', '2014')
+prepare_roidb(imdb)
+roidb = imdb.roidb
+mean, std = add_bbox_regression_targets(roidb)
+
+with open('rois_coco_train_2014.txt', 'w') as f:
+    for image_index in xrange(len(roidb)):
+        if image_index % 1000 == 0:
+            print '{}/{}'.format(image_index, len(roidb))
+        box = roidb[image_index]
+        # image_index img_path channels height width
+        f.write('# {}\n{}\n{}\n{}\n{}\n'.format(
+            image_index, box['image'], box['channel'], box['height'], box['width']))
+        # flipped
+        f.write('{}\n'.format(0))
+        num_windows = box['boxes'].shape[0]
+        # num_windows
+        f.write('{}\n'.format(num_windows))
+        for k in xrange(num_windows):
+            class_index = int(box['bbox_targets'][k, 0])
+            # class_index
+            f.write('{} '.format(class_index))
+            x1 = box['boxes'][k, 0]
+            y1 = box['boxes'][k, 1]
+            x2 = box['boxes'][k, 2]
+            y2 = box['boxes'][k, 3]
+            # overlap
+            overlap = box['max_overlaps'][k]
+            f.write('{0:.2f} '.format(overlap))
+            # x1 y1 x2 y2
+            f.write('{} {} {} {} '.format(x1, y1, x2, y2))
+            dx = box['bbox_targets'][k, 1]
+            dy = box['bbox_targets'][k, 2]
+            dw = box['bbox_targets'][k, 3]
+            dh = box['bbox_targets'][k, 4]
+            # dx dy dw dh
+            f.write('%.2f %.2f %.2f %.2f\n' % (dx, dy, dw, dh))
