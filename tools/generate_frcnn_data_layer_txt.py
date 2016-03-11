fc61334404fa3637b57a1291e4fca7d4bf33be58
Kun Wang <wk910930@gmail.com>
2016-03-11 11:44:00 +0800

    check num_windows and channels

--- tools/generate_frcnn_data_layer_txt.py
+++ tools/generate_frcnn_data_layer_txt.py
@@ -13,22 +13,27 @@ from datasets.coco import coco
 from roi_data_layer.roidb import prepare_roidb
 from roi_data_layer.roidb import add_bbox_regression_targets
 
-imdb = coco('minival', '2014')
+imdb = coco('train', '2014')
 prepare_roidb(imdb)
 roidb = imdb.roidb
 mean, std = add_bbox_regression_targets(roidb)
 
-with open('rois_coco_minival_2014.txt', 'w') as f:
+with open('rois_coco_train_2014.txt', 'w') as f:
     for image_index in xrange(len(roidb)):
         if image_index % 1000 == 0:
             print '{}/{}'.format(image_index, len(roidb))
         box = roidb[image_index]
+        num_windows = box['boxes'].shape[0]
+        if num_windows == 0:
+            print '{} does not have proposals!'.format(box['image'])
+            continue
+        if box['channel'] not in (1, 3):
+            print '{} has strange channels[{}]!'.format(box['image'], box['channel'])
         # image_index img_path channels height width
         f.write('# {}\n{}\n{}\n{}\n{}\n'.format(
             image_index, box['image'], box['channel'], box['height'], box['width']))
         # flipped
         f.write('{}\n'.format(0))
-        num_windows = box['boxes'].shape[0]
         # num_windows
         f.write('{}\n'.format(num_windows))
         class_index = box['max_classes']
