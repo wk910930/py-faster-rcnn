b1416cd5e54d9b7b5474a10aaa429148caae70e7
Kun Wang <wk910930@gmail.com>
2016-03-19 21:16:53 +0800

    update generate_frcnn_data_layer_txt

--- tools/generate_frcnn_data_layer_txt.py
+++ tools/generate_frcnn_data_layer_txt.py
@@ -14,9 +14,22 @@ from datasets.coco import coco
 from roi_data_layer.roidb import prepare_roidb
 from roi_data_layer.roidb import add_bbox_regression_targets
 
-imdb = coco('train', '2014')
+image_set = 'val'
+year = '2014'
+
+imdb = coco(image_set, year)
+imdb.set_proposal_method('mcg')
 prepare_roidb(imdb)
 roidb = imdb.roidb
+
+# filter roidb
+remove_indices = []
+for i in xrange(len(roidb)):
+    if roidb[i]['boxes'].shape[0] == 0:
+        remove_indices.append(i)
+roidb = [i for j, i in enumerate(roidb) if j not in remove_indices]
+print '{} images are filtered'.format(len(remove_indices))
+
 mean, std = add_bbox_regression_targets(roidb)
 
 with open('bbox_means.pkl', 'wb') as fid1:
@@ -24,40 +37,39 @@ with open('bbox_means.pkl', 'wb') as fid1:
 with open('bbox_stds.pkl', 'wb') as fid2:
     cPickle.dump(std, fid2, cPickle.HIGHEST_PROTOCOL)
 
-with open('rois_coco_train_2014.txt', 'w') as f:
+with open('rois_coco_' + image_set + '_' + year + '.txt', 'w') as f:
     for image_index in xrange(len(roidb)):
         if image_index % 1000 == 0:
             print '{}/{}'.format(image_index, len(roidb))
-        box = roidb[image_index]
-        num_windows = box['boxes'].shape[0]
-        if num_windows == 0:
-            print '{} does not have proposals!'.format(box['image'])
-            continue
-        if box['channel'] not in (1, 3):
-            print '{} has strange channels[{}]!'.format(box['image'], box['channel'])
+
+        # load next item
+        item = roidb[image_index]
+        if item['channel'] not in (1, 3):
+            print '{} has strange channels[{}]!'.format(item['image'], item['channel'])
         # image_index img_path channels height width
         f.write('# {}\n{}\n{}\n{}\n{}\n'.format(
-            image_index, box['image'], box['channel'], box['height'], box['width']))
+            image_index, item['image'], item['channel'], item['height'], item['width']))
         # flipped
         f.write('{}\n'.format(0))
         # num_windows
+        num_windows = item['boxes'].shape[0]
         f.write('{}\n'.format(num_windows))
-        class_index = box['max_classes']
-        overlap = box['max_overlaps']
+        class_index = item['max_classes']
+        overlap = item['max_overlaps']
         for k in xrange(num_windows):
             # class_index
             f.write('{} '.format(class_index[k]))
-            x1 = box['boxes'][k, 0]
-            y1 = box['boxes'][k, 1]
-            x2 = box['boxes'][k, 2]
-            y2 = box['boxes'][k, 3]
             # overlap
             f.write('%.2f ' % overlap[k])
             # x1 y1 x2 y2
+            x1 = item['boxes'][k, 0]
+            y1 = item['boxes'][k, 1]
+            x2 = item['boxes'][k, 2]
+            y2 = item['boxes'][k, 3]
             f.write('{} {} {} {} '.format(x1, y1, x2, y2))
-            dx = box['bbox_targets'][k, 1]
-            dy = box['bbox_targets'][k, 2]
-            dw = box['bbox_targets'][k, 3]
-            dh = box['bbox_targets'][k, 4]
             # dx dy dw dh
+            dx = item['bbox_targets'][k, 1]
+            dy = item['bbox_targets'][k, 2]
+            dw = item['bbox_targets'][k, 3]
+            dh = item['bbox_targets'][k, 4]
             f.write('%.2f %.2f %.2f %.2f\n' % (dx, dy, dw, dh))
