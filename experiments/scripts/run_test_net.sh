252365cbb95b48beb7ad879ba416583cc2100999
wang kun <wk910930@gmail.com>
2016-09-19 10:14:32 +0800

    use bn_liuyu as default setting to test environment

--- experiments/scripts/run_test_net.sh
+++ experiments/scripts/run_test_net.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ./tools/test_net.py --gpu 0 \
-     --def test.prototxt \
-     --net model.caffemodel \
+     --def /home/kwang/model_zoo/detections/bn_liuyu/deploy.prototxt \
+     --net /home/kwang/model_zoo/detections/bn_liuyu/models/hkbn_4d_fast_rcnn_iter_120000.caffemodel \
      --imdb ilsvrc_2013_val2 \
      --comp \
      --cfg ./experiments/cfgs/frcnn.yml \
-     --bbox_mean bbox_means.pkl \
-     --bbox_std bbox_stds.pkl
+     --bbox_mean /home/kwang/proposals/ilsvrc/liuyu/bbox_means.pkl \
+     --bbox_std /home/kwang/proposals/ilsvrc/liuyu/bbox_stds.pkl
