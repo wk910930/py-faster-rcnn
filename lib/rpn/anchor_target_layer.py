597e8c6211d15d0345625a0a7e4a7b37323cc985
Kun Wang <wk910930@gmail.com>
2016-11-16 22:30:41 +0800

    support setting anchor_base_size and anchor_ratios in anchor_target_layer

--- lib/rpn/anchor_target_layer.py
+++ lib/rpn/anchor_target_layer.py
@@ -25,8 +25,11 @@ class AnchorTargetLayer(caffe.Layer):
 
     def setup(self, bottom, top):
         layer_params = yaml.load(self.param_str_)
+        anchor_base_size = layer_params.get('base_size', 16)
+        anchor_ratios = layer_params.get('ratios', (0.5, 1, 2))
         anchor_scales = layer_params.get('scales', (8, 16, 32))
-        self._anchors = generate_anchors(scales=np.array(anchor_scales))
+        self._anchors = generate_anchors(base_size=anchor_base_size, \
+            ratios=np.array(anchor_ratios), scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
         self._feat_stride = layer_params['feat_stride']
 
