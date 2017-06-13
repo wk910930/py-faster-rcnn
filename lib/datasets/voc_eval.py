b1fdb9e817a930f1df4857dabd5dd31745e430f2
wang kun <wk910930@gmail.com>
2017-06-13 21:21:27 +0800

    Remove pose and truncated

--- lib/datasets/voc_eval.py
+++ lib/datasets/voc_eval.py
@@ -16,8 +16,6 @@ def parse_rec(filename):
     for obj in tree.findall('object'):
         obj_struct = {}
         obj_struct['name'] = obj.find('name').text
-        obj_struct['pose'] = obj.find('pose').text
-        obj_struct['truncated'] = int(obj.find('truncated').text)
         obj_struct['difficult'] = int(obj.find('difficult').text)
         bbox = obj.find('bndbox')
         obj_struct['bbox'] = [int(bbox.find('xmin').text),
