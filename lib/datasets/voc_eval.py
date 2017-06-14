d1ab215de70a91412499b8f8d21e6f7922d84646
wang kun <wk910930@gmail.com>
2017-06-14 22:53:07 +0800

    Convert to lower case when eval

--- lib/datasets/voc_eval.py
+++ lib/datasets/voc_eval.py
@@ -121,7 +121,7 @@ def voc_eval(detpath,
     class_recs = {}
     npos = 0
     for imagename in imagenames:
-        R = [obj for obj in recs[imagename] if obj['name'] == classname]
+        R = [obj for obj in recs[imagename] if obj['name'].lower().strip() == classname]
         bbox = np.array([x['bbox'] for x in R])
         difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
         det = [False] * len(R)
