9bda749c889a6cd4ac784eb7fffaffc8c7b12643
wangkun <wangkun@sensetime.com>
2017-06-30 14:55:24 +0800

    Add prep_im_for_blob() and prep_im_for_blob_cfm()

--- lib/utils/blob.py
+++ lib/utils/blob.py
@@ -65,3 +65,57 @@ def prep_im_for_blob(im, pixel_means, target_size, max_size):
                     interpolation=cv2.INTER_LINEAR)
 
     return im, im_scale
+
+def prep_im_for_blob_cfm(im, input_scales):
+    """Converts an image into a network input.
+    Arguments:
+        im (ndarray): a color image in BGR order
+    Returns:
+        blob (ndarray): a data blob holding an image pyramid
+        im_scale_factors (list): list of image scales (relative to im) used
+            in the image pyramid
+    """
+    im_orig = im.astype(np.float32, copy=True)
+    im_orig -= cfg.PIXEL_MEANS
+
+    im_shape = im_orig.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+
+    processed_ims = []
+    im_scale_factors = []
+
+    for target_size in input_scales:
+        im_scale = float(target_size) / float(im_size_min)
+        # Prevent the biggest axis from being more than MAX_SIZE
+        if np.round(im_scale * im_size_max) > cfg.TEST.MAX_SIZE:
+            im_scale = float(cfg.TEST.MAX_SIZE) / float(im_size_max)
+        im = cv2.resize(im_orig, None, None, fx=im_scale, fy=im_scale,
+                        interpolation=cv2.INTER_LINEAR)
+        im_scale_factors.append(im_scale)
+        processed_ims.append(im)
+
+    # Create a blob to hold the input images
+    blob = im_list_to_blob(processed_ims)
+
+    return blob, np.array(im_scale_factors)
+
+def pred_rois_for_blob(im_rois, im_scales):
+    """
+    Convert rois to network input
+    support multi-scale testing
+    """
+    im_rois = im_rois.astype(np.float, copy=False)
+    if len(im_scales) > 1:
+        widths = im_rois[:, 2] - im_rois[:, 0] + 1
+        heights = im_rois[:, 3] - im_rois[:, 1] + 1
+
+        areas = widths * heights
+        scaled_areas = areas[:, np.newaxis] * (im_scales[np.newaxis, :] ** 2)
+        diff_areas = np.abs(scaled_areas - 224 * 224)
+        levels = diff_areas.argmin(axis=1)[:, np.newaxis]
+    else:
+        levels = np.zeros((im_rois.shape[0], 1), dtype=np.int)
+    im_rois = im_rois * im_scales[levels]
+    rois_blob = np.hstack((levels.astype(np.float), im_rois))
+    return rois_blob
