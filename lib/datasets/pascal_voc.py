6b5e19a58c2d15e19a38b69a6287ae5eea803196
Kun Wang <wk910930@gmail.com>
2017-07-06 19:52:32 +0800

    Uncomment print code

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -256,9 +256,8 @@ class pascal_voc(imdb):
             # Exclude the samples labeled as difficult
             non_diff_objs = [
                 obj for obj in objs if int(obj.find('difficult').text) == 0]
-            # if len(non_diff_objs) != len(objs):
-            #     print 'Removed {} difficult objects'.format(
-            #         len(objs) - len(non_diff_objs))
+            if len(non_diff_objs) != len(objs):
+                print 'Removed {} difficult objects'.format(len(objs) - len(non_diff_objs))
             objs = non_diff_objs
         num_objs = len(objs)
 
