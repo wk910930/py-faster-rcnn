8673217814d8850a8f54ebfd7984cdf87767830e
Kun Wang <wk910930@gmail.com>
2017-07-06 23:33:04 +0800

    Update pascal_voc_seg.py

--- lib/datasets/pascal_voc_seg.py
+++ lib/datasets/pascal_voc_seg.py
@@ -33,21 +33,33 @@ class pascal_voc_seg(pascal_voc):
                        'use_diff': False,
                        'matlab_eval': False,
                        'rpn_file': None}
-        self._data_path = os.path.join(self._devkit_path)
+        self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
         self._roidb_path = os.path.join(self.cache_path, 'voc_2012_' + image_set + '_mcg_maskdb')
 
     def image_path_at(self, i):
-        image_path = os.path.join(self._data_path, 'img', self._image_index[i] + self._image_ext)
+        image_path = os.path.join(self._data_path, 'SBD', 'img', self._image_index[i] + self._image_ext)
         assert os.path.exists(image_path), 'Path does not exist: {}'.format(image_path)
         return image_path
 
-    def roidb_path_at(self, i):
-        if i >= self._ori_image_num:
-            return os.path.join(self._roidb_path,
-                                self.image_index[i % self._ori_image_num] + '_flip.mat')
-        else:
-            return os.path.join(self._roidb_path,
-                                self.image_index[i] + '.mat')
+    def gt_roidb(self):
+        """
+        Return the database of ground-truth regions of interest.
+
+        This function loads/saves from/to a cache file to speed up future calls.
+        """
+        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb.pkl')
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as fid:
+                roidb = cPickle.load(fid)
+            print '{} gt roidb loaded from {}'.format(self.name, cache_file)
+            return roidb
+        num_image = len(self.image_index)
+        gt_roidb = [self._load_sbd_annotations(index) for index in xrange(num_image)]
+        with open(cache_file, 'wb') as fid:
+            cPickle.dump(gt_roidb, fid, cPickle.HIGHEST_PROTOCOL)
+        print 'wrote gt roidb to {}'.format(cache_file)
+
+        return gt_roidb
 
     def gt_maskdb(self):
         cache_file = os.path.join(self.cache_path, self.name + '_gt_maskdb.pkl')
@@ -79,6 +91,45 @@ class pascal_voc_seg(pascal_voc):
             image_index = [x.strip() for x in f.readlines()]
         return image_index
 
+    def _load_sbd_annotations(self, index):
+        if index % 1000 == 0:
+            print '%d / %d' % (index, len(self._image_index))
+        image_name = self._image_index[index]
+        inst_file_name = os.path.join(self._data_path,  'SBD', 'inst', image_name + '.mat')
+        gt_inst_mat = scipy.io.loadmat(inst_file_name)
+        gt_inst_data = gt_inst_mat['GTinst']['Segmentation'][0][0]
+        unique_inst = np.unique(gt_inst_data)
+        background_ind = np.where(unique_inst == 0)[0]
+        unique_inst = np.delete(unique_inst, background_ind)
+
+        cls_file_name = os.path.join(self._data_path,  'SBD', 'cls', image_name + '.mat')
+        gt_cls_mat = scipy.io.loadmat(cls_file_name)
+        gt_cls_data = gt_cls_mat['GTcls']['Segmentation'][0][0]
+
+        boxes = np.zeros((len(unique_inst), 4), dtype=np.uint16)
+        gt_classes = np.zeros(len(unique_inst), dtype=np.int32)
+        overlaps = np.zeros((len(unique_inst), self.num_classes), dtype=np.float32)
+        for ind, inst_mask in enumerate(unique_inst):
+            im_mask = (gt_inst_data == inst_mask)
+            im_cls_mask = np.multiply(gt_cls_data, im_mask)
+            unique_cls_inst = np.unique(im_cls_mask)
+            background_ind = np.where(unique_cls_inst == 0)[0]
+            unique_cls_inst = np.delete(unique_cls_inst, background_ind)
+            assert len(unique_cls_inst) == 1
+            gt_classes[ind] = unique_cls_inst[0]
+            [r, c] = np.where(im_mask > 0)
+            boxes[ind, 0] = np.min(c)
+            boxes[ind, 1] = np.min(r)
+            boxes[ind, 2] = np.max(c)
+            boxes[ind, 3] = np.max(r)
+            overlaps[ind, unique_cls_inst[0]] = 1.0
+
+        overlaps = scipy.sparse.csr_matrix(overlaps)
+        return {'boxes': boxes,
+              'gt_classes': gt_classes,
+              'gt_overlaps': overlaps,
+              'flipped': False}
+
     def _load_sbd_mask_annotations(self, index, gt_roidbs):
         """
         Load gt_masks information from SBD's additional data
@@ -86,14 +137,14 @@ class pascal_voc_seg(pascal_voc):
         if index % 1000 == 0:
             print '%d / %d' % (index, len(self._image_index))
         image_name = self._image_index[index]
-        inst_file_name = os.path.join(self._data_path, 'inst', image_name + '.mat')
+        inst_file_name = os.path.join(self._data_path, 'SBD', 'inst', image_name + '.mat')
         gt_inst_mat = scipy.io.loadmat(inst_file_name)
         gt_inst_data = gt_inst_mat['GTinst']['Segmentation'][0][0]
         unique_inst = np.unique(gt_inst_data)
         background_ind = np.where(unique_inst == 0)[0]
         unique_inst = np.delete(unique_inst, background_ind)
         gt_roidb = gt_roidbs[index]
-        cls_file_name = os.path.join(self._data_path, 'cls', image_name + '.mat')
+        cls_file_name = os.path.join(self._data_path, 'SBD',  'cls', image_name + '.mat')
         gt_cls_mat = scipy.io.loadmat(cls_file_name)
         gt_cls_data = gt_cls_mat['GTcls']['Segmentation'][0][0]
         gt_masks = []
@@ -119,42 +170,22 @@ class pascal_voc_seg(pascal_voc):
         }
 
     def append_flipped_masks(self):
-        """
-        This method is only accessed when we use maskdb, so implement here
-        Append flipped images to mask database
-        Note this method doesn't actually flip the 'image', it flip masks instead
-        """
-        cache_file = os.path.join(self.cache_path, self.name + '_' + cfg.TRAIN.PROPOSAL_METHOD + '_maskdb_flip.pkl')
-        if os.path.exists(cache_file):
-            with open(cache_file, 'rb') as fid:
-                flip_maskdb = cPickle.load(fid)
-            print '{} gt flipped roidb loaded from {}'.format(self.name, cache_file)
-            self.maskdb.extend(flip_maskdb)
-            # Need to check this condition since otherwise we may occasionally *4
-            if self._image_index == self.num_images:
-                self._image_index *= 2
-        else:
-            # pure image number hold for future development
-            # this is useless since append flip mask will only be called once
-            num_images = self._ori_image_num
-            flip_maskdb = []
-            for i in xrange(num_images):
-                masks = self.maskdb[i]['gt_masks']
-                masks_flip = []
-                for mask_ind in xrange(len(masks)):
-                    mask_flip = np.fliplr(masks[mask_ind])
-                    masks_flip.append(mask_flip)
-                entry = {'gt_masks': masks_flip,
-                         'mask_max': self.maskdb[i]['mask_max'],
-                         'flipped': True}
-                flip_maskdb.append(entry)
-            with open(cache_file, 'wb') as fid:
-                cPickle.dump(flip_maskdb, fid, cPickle.HIGHEST_PROTOCOL)
-            print 'wrote gt flipped maskdb to {}'.format(cache_file)
-            self.maskdb.extend(flip_maskdb)
-            # Need to check this condition since otherwise we may occasionally *4
-            if self._image_index == self.num_images:
-                self._image_index *= 2
+        num_images = self._ori_image_num
+        flip_maskdb = []
+        for i in xrange(num_images):
+            masks = self.maskdb[i]['gt_masks']
+            masks_flip = []
+            for mask_ind in xrange(len(masks)):
+                mask_flip = np.fliplr(masks[mask_ind])
+                masks_flip.append(mask_flip)
+            entry = {'gt_masks': masks_flip,
+                     'mask_max': self.maskdb[i]['mask_max'],
+                     'flipped': True}
+            flip_maskdb.append(entry)
+        self.maskdb.extend(flip_maskdb)
+        # Need to check this condition since otherwise we may occasionally *4
+        if self._image_index == self.num_images:
+            self._image_index *= 2
 
     def visualization_segmentation(self, output_dir):
         vis_seg(self.image_index, self.classes, output_dir, self._data_path)
