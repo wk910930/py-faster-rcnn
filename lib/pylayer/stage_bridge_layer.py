6e71312b45e4bc333c4384b9b2c530e707c0540e
wangkun <wangkun@sensetime.com>
2017-07-14 19:17:48 +0800

    Update stage_bridge_layer.py

--- lib/pylayer/stage_bridge_layer.py
+++ lib/pylayer/stage_bridge_layer.py
@@ -115,10 +115,9 @@ class StageBridgeLayer(caffe.Layer):
         )
         all_rois[:, 1:5], self._clip_keep = clip_boxes_mnc(all_rois[:, 1:5], im_info[:2])
 
-        labels, rois_out, fg_inds, keep_inds, mask_targets, top_mask_info, bbox_targets, bbox_inside_weights = \
-            self._sample_output(all_rois, gt_boxes, im_info[2], gt_masks, mask_info)
+        labels, rois_out, fg_inds, mask_targets, top_mask_info, bbox_targets, bbox_inside_weights = \
+            _sample_output(all_rois, gt_boxes, im_info[2], gt_masks, mask_info, self._num_classes)
         bbox_outside_weights = np.array(bbox_inside_weights > 0).astype(np.float32)
-        self._keep_inds = keep_inds
 
         mask_weight = np.zeros((rois_out.shape[0], 1, cfg.MASK_SIZE, cfg.MASK_SIZE))
         mask_weight[0:len(fg_inds), :, :, :] = 1
@@ -134,55 +133,6 @@ class StageBridgeLayer(caffe.Layer):
             'bbox_outside_weights': bbox_outside_weights
         }
         return blobs
-
-    def _sample_output(self, all_rois, gt_boxes, im_scale, gt_masks, mask_info):
-        overlaps = bbox_overlaps(
-            np.ascontiguousarray(all_rois[:, 1:5], dtype=np.float),
-            np.ascontiguousarray(gt_boxes[:, :4], dtype=np.float))
-        gt_assignment = overlaps.argmax(axis=1)
-        max_overlaps = overlaps.max(axis=1)
-        labels = gt_boxes[gt_assignment, 4]
-        # Sample foreground indexes
-        fg_inds = np.where(max_overlaps >= cfg.TRAIN.BBOX_THRESH)[0]
-        bg_inds = np.where(max_overlaps < cfg.TRAIN.BBOX_THRESH)[0]
-        keep_inds = np.append(fg_inds, bg_inds).astype(int)
-        # Select sampled values from various arrays:
-        labels = labels[keep_inds]
-        # Clamp labels for the background RoIs to 0
-        labels[len(fg_inds):] = 0
-        rois = all_rois[keep_inds]
-
-        bbox_target_data = _compute_targets(
-        rois[:, 1:5], gt_boxes[gt_assignment[keep_inds], :4], labels)
-
-        bbox_targets, bbox_inside_weights = _get_bbox_regression_labels(
-            bbox_target_data, self._num_classes)
-
-        scaled_rois = rois[:, 1:5] / float(im_scale)
-        scaled_gt_boxes = gt_boxes[:, :4] / float(im_scale)
-
-        pos_masks = np.zeros((len(keep_inds), 1,  cfg.MASK_SIZE,  cfg.MASK_SIZE))
-        top_mask_info = np.zeros((len(keep_inds), 12))
-        top_mask_info[len(fg_inds):, :] = -1
-
-        for i, val in enumerate(fg_inds):
-            gt_box = scaled_gt_boxes[gt_assignment[val]]
-            gt_box = np.around(gt_box).astype(int)
-            ex_box = np.around(scaled_rois[i]).astype(int)
-            gt_mask = gt_masks[gt_assignment[val]]
-            gt_mask_info = mask_info[gt_assignment[val]]
-            gt_mask = gt_mask[0:gt_mask_info[0], 0:gt_mask_info[1]]
-            # regression targets is the intersection of bounding box and gt mask
-            ex_mask = intersect_mask(ex_box, gt_box, gt_mask)
-            pos_masks[i, ...] = ex_mask
-            top_mask_info[i, 0] = gt_assignment[val]
-            top_mask_info[i, 1] = gt_mask_info[0]
-            top_mask_info[i, 2] = gt_mask_info[1]
-            top_mask_info[i, 3] = labels[i]
-            top_mask_info[i, 4:8] = ex_box
-            top_mask_info[i, 8:12] = gt_box
-
-        return labels, rois, fg_inds, keep_inds, pos_masks, top_mask_info, bbox_targets, bbox_inside_weights
 
     def forward_test(self, bottom, top):
         rois = bottom[0].data
@@ -243,3 +193,52 @@ def _compute_targets(ex_rois, gt_rois, labels):
                 / np.array(cfg.TRAIN.BBOX_NORMALIZE_STDS))
     return np.hstack(
             (labels[:, np.newaxis], targets)).astype(np.float32, copy=False)
+
+def _sample_output(all_rois, gt_boxes, im_scale, gt_masks, mask_info, num_classes):
+    overlaps = bbox_overlaps(
+        np.ascontiguousarray(all_rois[:, 1:5], dtype=np.float),
+        np.ascontiguousarray(gt_boxes[:, :4], dtype=np.float))
+    gt_assignment = overlaps.argmax(axis=1)
+    max_overlaps = overlaps.max(axis=1)
+    labels = gt_boxes[gt_assignment, 4]
+    # Sample foreground indexes
+    fg_inds = np.where(max_overlaps >= cfg.TRAIN.BBOX_THRESH)[0]
+    bg_inds = np.where(max_overlaps < cfg.TRAIN.BBOX_THRESH)[0]
+    keep_inds = np.append(fg_inds, bg_inds).astype(int)
+    # Select sampled values from various arrays:
+    labels = labels[keep_inds]
+    # Clamp labels for the background RoIs to 0
+    labels[len(fg_inds):] = 0
+    rois = all_rois[keep_inds]
+
+    bbox_target_data = _compute_targets(
+        rois[:, 1:5], gt_boxes[gt_assignment[keep_inds], :4], labels)
+
+    bbox_targets, bbox_inside_weights = \
+         _get_bbox_regression_labels(bbox_target_data, num_classes)
+
+    scaled_rois = rois[:, 1:5] / float(im_scale)
+    scaled_gt_boxes = gt_boxes[:, :4] / float(im_scale)
+
+    pos_masks = np.zeros((len(keep_inds), 1,  cfg.MASK_SIZE,  cfg.MASK_SIZE))
+    top_mask_info = np.zeros((len(keep_inds), 12))
+    top_mask_info[len(fg_inds):, :] = -1
+
+    for i, val in enumerate(fg_inds):
+        gt_box = scaled_gt_boxes[gt_assignment[val]]
+        gt_box = np.around(gt_box).astype(int)
+        ex_box = np.around(scaled_rois[i]).astype(int)
+        gt_mask = gt_masks[gt_assignment[val]]
+        gt_mask_info = mask_info[gt_assignment[val]]
+        gt_mask = gt_mask[0:gt_mask_info[0], 0:gt_mask_info[1]]
+        # regression targets is the intersection of bounding box and gt mask
+        ex_mask = intersect_mask(ex_box, gt_box, gt_mask)
+        pos_masks[i, ...] = ex_mask
+        top_mask_info[i, 0] = gt_assignment[val]
+        top_mask_info[i, 1] = gt_mask_info[0]
+        top_mask_info[i, 2] = gt_mask_info[1]
+        top_mask_info[i, 3] = labels[i]
+        top_mask_info[i, 4:8] = ex_box
+        top_mask_info[i, 8:12] = gt_box
+
+    return labels, rois, fg_inds, pos_masks, top_mask_info, bbox_targets, bbox_inside_weights
